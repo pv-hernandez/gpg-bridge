8e44b02f9ebc7d6fbf3cdb92735181d8952c3c6f
Paulo Hernandez <paulovitormsh@hotmail.com>
2026-07-21 23:19:51 -0300

    add flake.nix to build for wsl

---
+++ flake.nix
@@ -0,0 +1,181 @@
+{
+  description = "GPG bridge for WSL";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = {nixpkgs, ...}: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      # "x86_64-darwin"
+      # "aarch64-darwin"
+    ];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs supportedSystems (
+        system:
+          f (
+            import nixpkgs {
+              inherit system;
+            }
+          )
+      );
+  in {
+    packages = forAllSystems (
+      pkgs: let
+        goArch = pkgs:
+          {
+            "x86_64" = "amd64";
+            "aarch64" = "arm64";
+          }.${
+            builtins.elemAt (pkgs.lib.strings.splitString "-" pkgs.stdenv.hostPlatform.system) 0
+          };
+        makeGoApp = goPkgs:
+          (goPkgs.buildGoModule
+            {
+              pname = "gpg-bridge";
+              version = "0.1.0";
+              src = ./.;
+
+              vendorHash = null;
+            }).overrideAttrs (oldAttrs: {
+            env =
+              (oldAttrs.env or {})
+              // {
+                GOOS = "windows";
+                GOARCH = goArch goPkgs;
+              };
+
+            postInstall = ''
+              mv "$out/bin/windows_${goArch goPkgs}/gpg-bridge.exe" "$out/bin/gpg-bridge.exe"
+              rm -d "$out/bin/windows_${goArch goPkgs}"
+            '';
+          });
+      in {
+        default = makeGoApp pkgs;
+      }
+    );
+
+    homeManagerModules.default = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: let
+      cfg = config.programs.gpg-bridge;
+      installTypes = {
+        gnupgStandalone = {
+          gpgconfCmd = "/mnt/c/Program Files/GnuPG/bin/gpgconf.exe";
+        };
+        gitForWindows = {
+          gpgconfCmd = "/mnt/c/Program Files/Git/bin/bash.exe";
+          gpgconfArg1 = "-c";
+          gpgconfArg2 =
+            /*
+            bash
+            */
+            ''
+              VALUE="$(gpgconf "$@")"
+              EXIT="$?"
+              if [ "$EXIT" -eq 0 ] && { [ "$1" = --list-dirs ] || [ "$1" = -L ]; } && [ -n "$2" ] && [ -n "$VALUE" ]; then
+                  cygpath -w "$VALUE"
+                  EXIT="$?"
+              else
+                  printf %s\\n "$VALUE"
+              fi
+              exit "$EXIT"
+            '';
+          gpgconfArg3 = "gpgconf";
+        };
+      };
+      genericInstallType = {
+        options = {
+          gpgconfCmd = lib.mkOption {type = lib.types.str;};
+          gpgconfArg1 = lib.mkOption {type = lib.nullOr lib.types.str;};
+          gpgconfArg2 = lib.mkOption {type = lib.nullOr lib.types.str;};
+          gpgconfArg3 = lib.mkOption {type = lib.nullOr lib.types.str;};
+          gpgconfArg4 = lib.mkOption {type = lib.nullOr lib.types.str;};
+          gpgconfArg5 = lib.mkOption {type = lib.nullOr lib.types.str;};
+          gpgconfArg6 = lib.mkOption {type = lib.nullOr lib.types.str;};
+          gpgconfArg7 = lib.mkOption {type = lib.nullOr lib.types.str;};
+          gpgconfArg8 = lib.mkOption {type = lib.nullOr lib.types.str;};
+          gpgconfArg9 = lib.mkOption {type = lib.nullOr lib.types.str;};
+        };
+      };
+      installType =
+        lib.types.coerceTo
+        (lib.types.enum (builtins.attrNames installTypes))
+        (s: installTypes.${s})
+        genericInstallType;
+
+      optionToEnv = name: "GPG_BRIDGE_${lib.strings.toUpper (lib.strings.toSnakeCase name)}";
+      optionToWslEnv = name: "${optionToEnv name}/w${
+        if (lib.strings.hasSuffix "Cmd" name)
+        then "p"
+        else ""
+      }";
+      mapInstallTypeAttrsToList = f: installConfig:
+        lib.mapAttrsToList f (lib.filterAttrs (_: v: v != null) installConfig);
+      gpgbridgeConfigText = installConfig:
+        lib.concatStringsSep "\n" (
+          (
+            mapInstallTypeAttrsToList
+            (key: value: "${optionToEnv key}=${lib.escapeShellArgument value}")
+            installConfig
+          )
+          // [
+            "WSLENV=${
+              lib.concatStringsSep
+              ":"
+              (
+                mapInstallTypeAttrsToList
+                (key: _: optionToWslEnv key)
+                installConfig
+              )
+            }"
+          ]
+        );
+      serviceTemplate = socketName:
+      /*
+      systemd
+      */
+      ''
+        [Unit]
+        Description=Windows bridge of GPG Agent's ${socketName} socket
+        Requires=${socketName}.socket
+        CollectMode=inactive-or-failed
+
+        [Service]
+        EnvironmentFile=%E/gpg-bridge/config.env
+        ExecStart="${cfg.package}/bin/gpg-bridge.exe" ${socketName}
+        StandardInput=socket
+        StandardOutput=socket
+        StandardError=journal
+      '';
+    in {
+      options = {
+        enable = lib.mkEnableOption "GPG Bridge";
+        package = lib.mkPackageOption pkgs "gpg-bridge" {};
+        gpgInstallType = lib.mkOption {
+          type = installType;
+          default = "gnupgStandalone";
+        };
+      };
+      config = lib.mkIf cfg.enable {
+        home.packages = [cfg.package];
+        xdg.configFile."systemd/user/gpg-agent-browser.socket".source = ./gpg-agent-browser.socket;
+        xdg.configFile."systemd/user/gpg-agent-browser@.service".text = serviceTemplate "gpg-agent-browser";
+        xdg.configFile."systemd/user/gpg-agent-extra.socket".source = ./gpg-agent-extra.socket;
+        xdg.configFile."systemd/user/gpg-agent-extra@.service".source = serviceTemplate "gpg-agent-extra";
+        xdg.configFile."systemd/user/gpg-agent-ssh.socket".source = ./gpg-agent-ssh.socket;
+        xdg.configFile."systemd/user/gpg-agent-ssh@.service".source = serviceTemplate "gpg-agent-ssh";
+        xdg.configFile."systemd/user/gpg-agent.socket".source = ./gpg-agent.socket;
+        xdg.configFile."systemd/user/gpg-agent@.service".source = serviceTemplate "gpg-agent";
+        xdg.configFile."gpg-bridge/config.env".text = gpgbridgeConfigText cfg.gpgInstallType;
+        programs.gpg.settings = {
+          "no-autostart" = true;
+        };
+      };
+    };
+  };
+}
